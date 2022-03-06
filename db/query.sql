fd409a59e679ff36dffbfa707b03c61359a8047a
NotANewt <meegafauna@gmail.com>
2022-03-06 16:20:42 -0600

    pseudocoded the query file

--- db/query.sql
+++ db/query.sql
@@ -29,7 +29,18 @@
     -- THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
 
 
+-- Bonus
+-- Try to add some additional functionality to your application, such as the ability to do the following:
 
+-- Update employee managers.
+
+-- View employees by manager.
+
+-- View employees by department.
+
+-- Delete departments, roles, and employees.
+
+-- View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
 
 
 
